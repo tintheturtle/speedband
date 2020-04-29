edcd734c3c6ad698fec335f1f98023f850ca6658
tintheturtle <tinhoang@bu.edu>
2020-04-29 11:47:59 -0400

    changed interface for net_io_counters in speed.py

--- speed.py
+++ speed.py
@@ -17,16 +17,16 @@ def calculateRates():
     ul: float = 0.00
     dl: float = 0.00
 
-    upload = psutil.net_io_counters(pernic=True)['en0'][0]
-    download = psutil.net_io_counters(pernic=True)['en0'][1]
+    upload = psutil.net_io_counters().bytes_sent
+    download = psutil.net_io_counters().bytes_recv
 
     up_down = (upload, download)
 
     while(True):
         last_up_down = up_down
 
-        upload = psutil.net_io_counters(pernic=True)['en0'][0]
-        download = psutil.net_io_counters(pernic=True)['en0'][1]
+        upload = psutil.net_io_counters().bytes_sent
+        download = psutil.net_io_counters().bytes_recv
 
         t1 = time.time()
         up_down = (upload, download)
@@ -37,8 +37,14 @@ def calculateRates():
         except:
             pass
 
-        if dl > 0.1 or ul >= 0.1:
-            time.sleep(5)
+        if dl > 2000 or ul > 2000:
             print('UL: {:0.2f} kB/s \n'.format(ul)+'DL: {:0.2f} kB/s'.format(dl))
+            print()
+
+        if dl > 0.1 or ul >= 0.1:
+            time.sleep(0.25)
 
 calculateRates()
+
+# Upload --> bytes_sent
+# Download --> bytes_received
